c0d47a1a402547b565a1ce6606b5bd4caf56ebaf
julibertoldi <bertoldijuliana1@gmail.com>
2025-10-08 17:52:52 -0300

    feat: agregar interfaz ICitySearchService y su implementación CitySearchService

---
+++ TravelBuddy/src/TravelBuddy.Application.Contracts/Cities/CityDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Cities
+{
+    public class CityDto
+    //formato de datos que la API va a devolver al front
+    {
+        public string Nombre { get; set; }
+        public string Pais { get; set; }
+        public string Region { get; set; }
+
+    }
+}
---
+++ TravelBuddy/src/TravelBuddy.Application.Contracts/Cities/ICitySearchService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Cities
+{
+    public interface ICitySearchService
+    {
+        Task<List<CityDto>> SearchCitiesByNameAsync(string partialName);
+    }
+}
