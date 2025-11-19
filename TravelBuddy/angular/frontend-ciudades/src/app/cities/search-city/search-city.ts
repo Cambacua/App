f4be3caeaccfb679660cbf56dfab0e6d4e707b00
julibertoldi <bertoldijuliana1@gmail.com>
2025-11-19 17:18:55 -0300

    Creacion de proyecto Angular, estructura de cities, y componentes city-search

---
+++ TravelBuddy/angular/frontend-ciudades/src/app/cities/search-city/search-city.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-city',
+  imports: [],
+  templateUrl: './search-city.html',
+  styleUrl: './search-city.css',
+})
+export class SearchCity {
+
+}
